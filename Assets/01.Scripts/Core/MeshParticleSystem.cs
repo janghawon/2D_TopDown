e71474c5fa6cc9d1f92d963815c37dbfe7352f71
VsPot07 <cocoacake84@gmail.com>
2023-04-04 19:00:55 +0900

    04/04 파티클 제작 중

--- Assets/01.Scripts/Core/MeshParticleSystem.cs
+++ Assets/01.Scripts/Core/MeshParticleSystem.cs
@@ -1,12 +1,13 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MeshParticleSystem : MonoBehaviour
 {
     private const int MAX_QUAD_AMOUNT = 15000;
 
-    [System.Serializable] 
+    [Serializable]
     public struct ParticleUVPixel
     {
         public Vector2Int uv00Pixel;
@@ -31,6 +32,11 @@ public class MeshParticleSystem : MonoBehaviour
     private Vector2[] _uv;
     private int[] _triangles;
 
+    private bool _updateVertices;
+    private bool _updateUV;
+    private bool _updateTriangles;
+
+
     private void Awake()
     {
         _mesh = new Mesh();
@@ -41,31 +47,38 @@ public class MeshParticleSystem : MonoBehaviour
         _mesh.vertices = _vertices;
         _mesh.uv = _uv;
         _mesh.triangles = _triangles;
+        //메시의 경계 바운드가 작으면 특정 좌표 이상 화면 밖으로 나가면 메시 전체가 그려지지 않는다.
+        // 따라서 경계 바운드를 넓혀줘야 한다.
+
+        _mesh.bounds = new Bounds(Vector3.zero, Vector3.one * 1000f); //너비 1000 X 1000 크기의 바운드 설정
 
         _meshFilter = GetComponent<MeshFilter>();
         _meshFilter.mesh = _mesh;
         _meshRenderer = GetComponent<MeshRenderer>();
 
         _meshRenderer.sortingLayerName = "Agent";
-        _meshRenderer.sortingOrder = 0;
+        _meshRenderer.sortingOrder = 0; //플레이어나 적군보다는 아래쪽에 그려지도록 함.
 
-        Texture mainTex = _meshRenderer.material.mainTexture;
+        Texture mainTex = _meshRenderer.material.mainTexture; // Diffuse에 넣어준 스프라이트가 가져와진다.
         int tWidth = mainTex.width;
         int tHeight = mainTex.height;
 
-        List<UVCoords> uvCoordList = new List<UVCoords>();
+        var uvCoordList = new List<UVCoords>();
 
-        foreach(ParticleUVPixel pixelUV in _uvPixelArr)
+        foreach (ParticleUVPixel pixelUV in _uvPixelArr)
         {
             UVCoords temp = new UVCoords
             {
-                uv00 = new Vector2((float)pixelUV.uv00Pixel.x / tWidth, (float)pixelUV.uv00Pixel.y / tHeight),
-                uv11 = new Vector2((float)pixelUV.uv11Pixel.x / tWidth, (float)pixelUV.uv11Pixel.y / tHeight),
+                uv00 = new Vector2((float)pixelUV.uv00Pixel.x / tWidth,
+                                    (float)pixelUV.uv00Pixel.y / tHeight),
+
+                uv11 = new Vector2((float)pixelUV.uv11Pixel.x / tWidth,
+                                    (float)pixelUV.uv11Pixel.y / tHeight)
             };
             uvCoordList.Add(temp);
         }
 
-        _uvCoordArr = uvCoordList.ToArray();
+        _uvCoordArr = uvCoordList.ToArray(); //배열로 변경된다.
     }
 
     public int GetRandomBloodIndex()
@@ -75,62 +88,72 @@ public class MeshParticleSystem : MonoBehaviour
 
     public int GetRandomShellIndex()
     {
-        return Random.Range(8, 9);
+        return Random.Range(8, 9); //확장성을 위해서 
     }
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(1))
         {
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             pos.z = 0;
-            AddQuad(pos, 0, new Vector3(1, 1, 0), false, GetRandomShellIndex());
+            Vector3 quadSize = new Vector3(1, 1, 0);
+            float rot = 0;
+            int uvIndex = GetRandomShellIndex();
+            int qIndex = AddQuad(pos, rot, quadSize, false, uvIndex);
+            FunctionUpdater.Instance.Create(() =>
+            {
+                pos += new Vector3(1, 1) * 0.8f * Time.deltaTime;
+                quadSize += new Vector3(1, 1) * Time.deltaTime; //초당 1씩 커지고
+                rot += 360f * Time.deltaTime;
+
+                UpdateQuad(qIndex, pos, rot, quadSize, false, uvIndex);
+            });
         }
-        else if(Input.GetMouseButtonDown(1))
-        {
-            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            pos.z = 0;
-            AddQuad(pos, 0, new Vector3(1, 1, 0), false, GetRandomBloodIndex());
-        }
+
     }
 
-    private int _quadIndex = 0;
+    private int _quadIndex = 0; //현재 쿼드의 인덱스
 
-    public int AddQuad(Vector3 pos, float rot, Vector3 quadSize, bool skwewd, int uvIndex)
+    public int AddQuad(Vector3 pos, float rot, Vector3 quadSize, bool skewed, int uvIndex)
     {
-        UpdateQuad(_quadIndex, pos, rot, quadSize, skwewd, uvIndex);
+        //여기에 실질적인 쿼드의 생성이 들어갈꺼고
+        UpdateQuad(_quadIndex, pos, rot, quadSize, skewed, uvIndex);
 
         int spawnedQuadIndex = _quadIndex;
-        _quadIndex = (_quadIndex + 1) % MAX_QUAD_AMOUNT;
+        _quadIndex = (_quadIndex + 1) % MAX_QUAD_AMOUNT; //최대치 초과하면 첫번째꺼부터 지워지게
 
         return spawnedQuadIndex;
     }
 
-    public void UpdateQuad(int quadIndex, Vector3 pos, float rot, Vector3 quadSize, bool skewd, int uvIndex)
+    public void UpdateQuad(int quadIndex, Vector3 pos, float rot, Vector3 quadSize, bool skewed, int uvIndex)
     {
         int vIndex0 = quadIndex * 4;
         int vIndex1 = vIndex0 + 1;
         int vIndex2 = vIndex0 + 2;
         int vIndex3 = vIndex0 + 3;
 
-        if(skewd)
+        if (skewed)
         {
-
+            //비틀기는 아직은 신경쓰지 말자 헷갈리거든
         }
         else
         {
-            _vertices[vIndex0] = pos + Quaternion.Euler(0, 0, rot - 180) * quadSize;
-            _vertices[vIndex1] = pos + Quaternion.Euler(0, 0, rot - 270) * quadSize;
-            _vertices[vIndex2] = pos + Quaternion.Euler(0, 0, rot - 0) * quadSize;
-            _vertices[vIndex3] = pos + Quaternion.Euler(0, 0, rot - 90) * quadSize;
+            _vertices[vIndex0] = pos + Quaternion.Euler(0, 0, rot - 180) * quadSize; // -1, -1
+            _vertices[vIndex1] = pos + Quaternion.Euler(0, 0, rot - 270) * quadSize; // -1, 1
+            _vertices[vIndex2] = pos + Quaternion.Euler(0, 0, rot - 0) * quadSize; // 1, 1
+            _vertices[vIndex3] = pos + Quaternion.Euler(0, 0, rot - 90) * quadSize; // 1, -1
         }
 
+        //7번인덱스의 핏자국 그려줘
+        // uv00, uv11
         UVCoords uv = _uvCoordArr[uvIndex];
         _uv[vIndex0] = uv.uv00;
         _uv[vIndex1] = new Vector2(uv.uv00.x, uv.uv11.y);
         _uv[vIndex2] = uv.uv11;
-        _uv[vIndex3] = new Vector2(uv.uv11.x, uv.uv11.y);
+        _uv[vIndex3] = new Vector2(uv.uv11.x, uv.uv00.y);
 
+        //삼각형 인덱스
         int tIndex = quadIndex * 6;
         _triangles[tIndex + 0] = vIndex0;
         _triangles[tIndex + 1] = vIndex1;
@@ -140,8 +163,52 @@ public class MeshParticleSystem : MonoBehaviour
         _triangles[tIndex + 4] = vIndex2;
         _triangles[tIndex + 5] = vIndex3;
 
-        _mesh.vertices = _vertices;
-        _mesh.uv = _uv;
-        _mesh.triangles = _triangles;
+        _updateVertices = true;
+        _updateUV = true;
+        _updateTriangles = true;
+        // ForceMeshUpdate
+    }
+
+    private void LateUpdate()
+    {
+        if (_updateVertices)
+        {
+            _mesh.vertices = _vertices;
+            _updateVertices = false;
+        }
+
+        if (_updateUV)
+        {
+            _mesh.uv = _uv;
+            _updateUV = false;
+        }
+
+        if (_updateTriangles)
+        {
+            _mesh.triangles = _triangles;
+            _updateTriangles = false;
+        }
+    }
+
+    public void DestroyQuad(int quadIndex)
+    {
+        int vIndex0 = quadIndex * 4;
+        int vIndex1 = vIndex0 + 1;
+        int vIndex2 = vIndex0 + 2;
+        int vIndex3 = vIndex0 + 3;
+
+        _vertices[vIndex0] = Vector3.zero;
+        _vertices[vIndex1] = Vector3.zero;
+        _vertices[vIndex2] = Vector3.zero;
+        _vertices[vIndex3] = Vector3.zero;
+
+        _updateVertices = true;
+    }
+
+    public void DestroyAllQuad()
+    {
+        Array.Clear(_vertices, 0, _vertices.Length);
+        _quadIndex = 0;
+        _updateVertices = true;
     }
 }
